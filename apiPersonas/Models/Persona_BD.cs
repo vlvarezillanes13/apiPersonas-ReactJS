13f0046510dc567e99e1170cc24018e445693f54
Vicente Álvarez <v.lvarez.illanes@gmail.com>
2022-06-20 13:51:00 -0400

    carpeta models -> clase según base

---
+++ apiPersonas/Models/Persona_BD.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apiPersonas.Models
+{
+    public class Persona_BD
+    {
+        [Key]
+        public int id { get; set; }
+        public string rut { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public int edad { get; set; }
+        public string email { get; set; }
+    }
+}
